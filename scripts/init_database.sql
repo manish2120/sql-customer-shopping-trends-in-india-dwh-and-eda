63936389f58834d2997fbab6602c2fd437a2a920
Manish Chavan <manish.chavan113@gmail.com>
2026-08-04 11:08:24 +0530

    update: rename database

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,11 +1,11 @@
 /* -------------------------------
 Script Purpose :
-	1. This script creates a new database named "CustomerShoppingTrendsDW" after checking if it already exists or not.
+	1. This script creates a new database named "IndianCustomerShoppingTrendsDW" after checking if it already exists or not.
 	2. If it does exists, the script will immediately drops the existing database and recreates it.
 	3. Additionally, script sets up three schemas within the database: 'bronze', 'silver' and 'gold'.
 
 Warning :
-	1. This script will drop the entire database named "CustomerShoppingTrendsDW" with the uncommited changes if the database exists.
+	1. This script will drop the entire database named "IndianCustomerShoppingTrendsDW" with the uncommited changes if the database exists.
 	2. All data inside the database will be permanently deleted.
 	3. Please ensure that you have a proper backup before running this script.
 ---------------------------------- */
@@ -14,20 +14,20 @@ Warning :
 USE master;
 GO
 
--- Drop and recreate the 'CustomerShoppingTrendsDW' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'CustomerShoppingTrendsDW')
+-- Drop and recreate the 'IndianCustomerShoppingTrendsDW' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'IndianCustomerShoppingTrendsDW')
 BEGIN
-	ALTER DATABASE CustomerShoppingTrendsDW SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE CustomerShoppingTrendsDW;
+	ALTER DATABASE IndianCustomerShoppingTrendsDW SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE IndianCustomerShoppingTrendsDW;
 END;
 GO
 
 -- Create the database
-CREATE DATABASE CustomerShoppingTrendsDW;
+CREATE DATABASE IndianCustomerShoppingTrendsDW;
 GO
 
--- Use the CustomerShoppingTrendsDW database
-USE CustomerShoppingTrendsDW;
+-- Use the IndianCustomerShoppingTrendsDW database
+USE IndianCustomerShoppingTrendsDW;
 GO
 
 -- Create Schemas : 'bronze', 'silver' and 'gold'
